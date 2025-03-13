0f6e7ae9f5a21fa3d74ce0943fdb508b5061fabd
Tomas Bosak <tombos255@gmail.com>
2025-03-13 15:05:23 +0100

    xfce4 settings

---
+++ Documents/BashScripts/PrepareStow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mv -v $HOME/.bashrc $HOME/.bashrc.bak
+mv -v $HOME/.bash_logout $HOME/.bash_logout.bak
+mv -v $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml.bak
