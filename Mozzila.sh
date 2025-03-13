d4224570873eb6c7c535cb8e05791f9d7a71f81a
Tomáš Bosák <85733005+TomBos@users.noreply.github.com>
2025-03-13 11:39:51 +0100

    Create Mozzila.sh

---
+++ Mozzila.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:mozillateam/ppa
+sudo apt update
+echo 'Package: *\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 1001' | sudo tee /etc/apt/preferences.d/mozilla-firefox  
+sudo apt install firefox
