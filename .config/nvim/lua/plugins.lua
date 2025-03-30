3b47832272b0853275a7a05bf13b47af6f675c1e
Tomáš Bosák <tombos255@gmail.com>
2025-03-30 15:57:04 +0200

    more nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,5 +4,7 @@ return {
   { "nvim-tree/nvim-tree.lua" },
   { "navarasu/onedark.nvim" },
   { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
+  
+  -- NeoTree plugin
+  require("config.neo_tree"),
 }
-
---
+++ Documents/BashScripts/LazyNvimDependencies.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo apt update -y && sudo apt install -y \
+  luarocks \
+  gcc \
+  fd-find
