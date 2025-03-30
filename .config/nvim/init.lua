202b8f1f91898823870497465174fd2b53b825fd
Tomáš Bosák <tombos255@gmail.com>
2025-03-30 14:04:24 +0200

    Nvim Telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,11 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 require("config.lazy")
+
+-- Color Scheme
+require('onedark').load()
+
+-- Telescope (File Search)
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,5 +2,7 @@ return {
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "nvim-lualine/lualine.nvim" },
   { "nvim-tree/nvim-tree.lua" },
+  { "navarasu/onedark.nvim" },
+  { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
 }
 
