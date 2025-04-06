49d90891cf208152879eff56e4468989afa3117b
Tomáš Bosák <tombos255@gmail.com>
2025-04-06 21:38:02 +0200

    more refactoring

--- .scripts/old/LazyNvimDependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sudo apt update -y && sudo apt install -y \
-  luarocks \
-  gcc \
-  fd-find \
-  ripgrep
-
--- .scripts/old/TerminalTheme.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-bash -c "$(wget -qO- https://git.io/vQgMr)"
