775cf865f21fa4c8d223847a75c9acb60defc31a
Tomáš Bosák <tombos255@gmail.com>
2025-04-06 20:49:20 +0200

    Refactoring folder structure

--- Documents/BashScripts/PrepareStow.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mv -v $HOME/.bashrc $HOME/.bashrc.bak
-mv -v $HOME/.bash_logout $HOME/.bash_logout.bak
-mv -v $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml.bak
