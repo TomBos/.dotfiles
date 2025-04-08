e523849dfe7afd5d28b3729e5f0576573a786af4
Tomáš Bosák <tombos255@gmail.com>
2025-04-08 11:46:27 +0200

    Syncing

--- .scripts/sys-migrations/1744097751.sh
+++ .scripts/sys-migrations/1744097751.sh
@@ -4,10 +4,20 @@ sudo pacman -S --noconfirm --needed filezilla \
   xclip \
   ruby \
   bash-completion 
-    
-  
-  
+      
 gem install neovim
 gem install environment
 
-gem environment | awk '/USER/ {print $5}'
+
+if [[ -f "lockfile.lock" ]]; then
+  source "lockfile.lock"
+else
+  # DO LATER!
+  echo "Lockfile not found. Creating it now..."
+fi
+
+GEM_BIN="$(gem environment | awk '/USER INSTALLATION DIRECTORY/ {print $5}')/bin"
+sed '/<ID:RUBY_PATH>/a export PATH="'"$GEM_BIN"':$PATH"' "$HOME/.bash_exports" > "$HOME/.bash_exports"
+
+
+
