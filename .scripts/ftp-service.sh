e8d268e1901912c2bc0d97bde1b25f17af44bf97
Tomáš Bosák <tombos255@gmail.com>
2025-05-13 09:12:42 +0200

    Updated FTP service

--- .scripts/ftp-service.sh
+++ .scripts/ftp-service.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
 LOCAL_DIR=/var/www/html/handel/presta-eshopy
-REMOTE_DIR=/mnt/ftp-server/htdocs
+REMOTE_DIR=/mnt/ftp-servers/chi/htdocs
 
 inotifywait -mrq -e close_write --format "%w%f" "$LOCAL_DIR" | while read file; do
     case "$file" in
-        *4913|*.swp|*.swo|*.tmp|*.lock|*/.git/*) continue ;;
+        *4913|*.swp|*.swo|*.tmp|*.lock|*/.git/*|*.*~) continue ;;
     esac
 
     # Make target path match local structure
     rel_path="${file#$LOCAL_DIR/}"
     dest_path="$REMOTE_DIR/$rel_path"
+
+    # Create dir if it doesnt exist
+    dest_dir=$(dirname "$dest_path")
+
+    if [ ! -d "$dest_dir" ]; then
+        mkdir -p "$dest_dir"
+    fi
 
     # Copy the file
     cp -f "$file" "$dest_path" && echo "✅ Synced: $rel_path"
