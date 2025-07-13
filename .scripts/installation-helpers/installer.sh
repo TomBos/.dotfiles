f8affaf507cf9c6dcffa730b21662de3ba33c19c
Tomáš Bosák <tombos255@gmail.com>
2025-07-14 00:29:52 +0200

    Cleaned up scripts dir

---
+++ .scripts/installation-helpers/installer.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Needed
+xorg-xinit xorg xorg-server xorg-apps 
+i3
+git
+kitty
+openssh
+librewolf-bin
+stow
+fastfetch
+imagemagick
+bash-completion
+xclip
+php
+xterm
+wget
+tree
+pavucontrol
+bluez blueman
+filezilla
+
+# Device specific
+brightnessctl
+
