b93faf55429200b5d9cf38cb3c0c89c2cb99c67f
Tomáš Bosák <tombos255@gmail.com>
2025-07-15 10:43:44 +0200

    XDG git clone

--- .scripts/git/git-post-clone.sh
+++ .scripts/git/git-post-clone.sh
@@ -3,7 +3,7 @@ set -e
 
 REPO_URL="$1"
 CUSTOM_DIR="$2"
-
+PROJECTS_DIR=${PROJECTS:-$HOME/src}
 
 # Determine the intended clone directory
 if [[ -n "$CUSTOM_DIR" ]]; then
@@ -17,7 +17,7 @@ fi
 command git clone "$REPO_URL" "$CLONE_DIR"
 
 
-if [[ "$CLONE_DIR" == "$HOME/Projects/handel/"* ]]; then
+if [[ "$CLONE_DIR" == "$PROJECTS_DIR/handel/"* ]]; then
 	# Set the template directory path
 	TEMPLATE_DIR="$HOME/git-templates/handel/hooks"
 	TARGET_DIR="$CLONE_DIR/.git/hooks"
