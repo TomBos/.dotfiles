f3693bc2bebcbb103c87c336e8da579c98b1b7da
Tomáš Bosák <tombos255@gmail.com>
2025-08-13 10:40:36 +0200

    Function rework

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,3 +6,20 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
 
+-- Disable arrow keys in normal mode
+map('n', '<Up>', '<Nop>', opts)
+map('n', '<Down>', '<Nop>', opts)
+map('n', '<Left>', '<Nop>', opts)
+map('n', '<Right>', '<Nop>', opts)
+
+-- Disable arrow keys in insert mode
+map('i', '<Up>', '<Nop>', opts)
+map('i', '<Down>', '<Nop>', opts)
+map('i', '<Left>', '<Nop>', opts)
+map('i', '<Right>', '<Nop>', opts)
+
+-- Disable arrow keys in visual mode
+map('v', '<Up>', '<Nop>', opts)
+map('v', '<Down>', '<Nop>', opts)
+map('v', '<Left>', '<Nop>', opts)
+map('v', '<Right>', '<Nop>', opts)
