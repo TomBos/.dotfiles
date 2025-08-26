fe04145e5fd4c613dbca28e1ed5cfab7a71ff404
Tomáš Bosák <tombos255@gmail.com>
2025-08-26 16:04:48 +0200

    Laptop settings

---
+++ program-check.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+ttf-poppins
+ttf-nerd-fonts-symbols
+swww
+ripgrep
+unzip
+librewolf-bin
+git
+kitty
+stow
+imagemagick
+bash-completion
+php
+filezilla
+hyprland
+tree
+neovim
+
+git clone https://aur.archlinux.org/nerd-fonts-jetbrains-mono.git
+cd nerd-fonts-jetbrains-mono
+makepkg -si
+
+paru
+
